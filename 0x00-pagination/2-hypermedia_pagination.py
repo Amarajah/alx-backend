92bdfc765788800f5732ade13e422d170e24978f
Amsrajah <naomi.ibeh69@gmail.com>
2023-07-30 11:36:00 +0000

    paginate

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -49,7 +49,7 @@ class Server:
         except IndexError:
             return []
 
-    def get_hyper(self, page:int = 1, page_size: int = 10) -> List[List]:
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
         "returns requested page"
         assert type(page) is int and page > 0
         assert type(page_size) is int and page_size > 0
