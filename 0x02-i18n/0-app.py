9b17c0be0c26c9c96211e3c1275c3b2ba1100c8a
Amsrajah <naomi.ibeh69@gmail.com>
2023-08-01 14:51:01 +0000

    index.html

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,7 +7,10 @@ app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def index():
+def index() -> str:
+    """
+    Handles route
+    """
     return render_template('0-index.html')
 
 if __name__ == "__main__":
